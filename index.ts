8fd08e5c0512dfe137538e65b4188e43c9cb3fa3
Tony Kharioki <antolisah@gmail.com>
2023-10-19 01:56:21 +0300

    bun serve

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+  fetch(req: Request): Response | Promise<Response> {
+    return new Response("Hello from Bun!!");
+  },
+
+  // Optional port number - the default value is 3000
+  port: 4000,
+});
+
+console.log(`Listening on port ${server.port}`);
