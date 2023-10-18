9f5ec54313667276c533333de3cdd0bf9bfa1bac
Tony Kharioki <antolisah@gmail.com>
2023-10-19 02:49:28 +0300

    bun simple routes

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 const server = Bun.serve({
   fetch(req: Request): Response | Promise<Response> {
-    return new Response("Hello from Bun!!!");
+    const url = new URL(req.url);
+    if (url.pathname === '/') return new Response('HomePage!');
+    if (url.pathname === '/blog') return new Response('Blog!');
+    return new Response('404!');
   },
 
   // Optional port number - the default value is 3000
