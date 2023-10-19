581a81538e386446a3b32c3eb5cfa7e4a0c9473f
Tony Kharioki <antolisah@gmail.com>
2023-10-19 03:21:31 +0300

    more file api functions

--- file.ts
+++ file.ts
@@ -1,3 +1,9 @@
-const data = 'I love Bun!';
+// const data = 'I love Bun!';
 
-await Bun.write('output.txt', data);
+// await Bun.write('output.txt', data);
+
+const file = await Bun.file('output.txt');
+console.log(await file.text());
+console.log(file.size);
+console.log(await file.stream());
+console.log(await file.arrayBuffer());
